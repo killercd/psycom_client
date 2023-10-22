68088bf6e462270ca2274231821158146908de37
killercd@gmail.com <killercd@gmail.com>
2023-10-22 15:35:11 +0200

    fast commit

--- psycomc.py
+++ psycomc.py
@@ -1,5 +1,9 @@
 import xmlrpc.client
 
 server = xmlrpc.client.ServerProxy("http://192.168.178.51:8000")
-result = server.new_session("FTPSESSION", "192.168.178.55")
+result = server.new_session("FTPSESSION", "192.168.178.55", "192.168.178.55", 21, 900)
+
 print(result)
+
+# data = server.get_session("FTPSESSION")
+# print(data)
